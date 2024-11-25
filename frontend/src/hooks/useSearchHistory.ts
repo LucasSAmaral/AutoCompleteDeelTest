bd8bdacfbdb55758054fb766adfd3128a43d3159
Lucas <lucassantosamaral@hotmail.com>
2024-11-24 23:35:04 -0300

    feat: create getSearchHistory to get the history and set it on localStorage

--- frontend/src/hooks/useSearchHistory.ts
+++ frontend/src/hooks/useSearchHistory.ts
@@ -20,6 +20,28 @@ export const useSearchHistory = () => {
     }
   }, []);
 
+  const getSearchHistory = useCallback(async () => {
+    try {
+      const response = await fetch('http://localhost:4000/api/autocomplete/history');
+
+      if (!response.ok) {
+        throw new Error('Error on posting searchHistory');
+      }
+
+      const searchHistoryResponse: string[] = await response.json();
+
+      setSearchHistory(searchHistoryResponse);
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (searchHistory.length === 0) {
+      getSearchHistory();
+    }
+  }, [searchHistory.length, getSearchHistory]);
+
   useEffect(() => {
     if (searchHistory.length % 5 === 0 && searchHistory.length > 0) {
       setLocalStorageHistory(searchHistory);
