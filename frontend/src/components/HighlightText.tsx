3c8bc04c47b2f5a5556a100f54472376fa61e1b2
Lucas <lucassantosamaral@hotmail.com>
2024-11-25 17:13:41 -0300

    chore: add comments to explain the redex logic

--- frontend/src/components/HighlightText.tsx
+++ frontend/src/components/HighlightText.tsx
@@ -8,15 +8,21 @@ interface HighlightTextProps {
 const HighlightText: React.FC<HighlightTextProps> = ({ suggestion, inputSearchValue }) => {
   if (!inputSearchValue) return <>{suggestion}</>;
 
+  // This Regex is going to match the inputSearchValue within the suggestion text.
+  // It captures the typed value, allowing flexible spacing and case-insensitive matches.
   const rgx = new RegExp(`\\b(${inputSearchValue.trim().replace(/\s+/g, '\\s*')})`, 'gi');
+
+  // Then the suggestion text is splitted into parts based on the regex.
   const repartedSuggestion = suggestion.split(rgx);
 
   return (
     <>
       {repartedSuggestion.map((part, index) =>
+        // If the typed value matches part of the suggestion text, it will highlight the matching portion.
         rgx.test(part) ? (
           <strong key={`highlighted-part-${index}`}>{part}</strong>
         ) : (
+          // I'm using React.Fragment instead of <></> to pass the key prop.
           <React.Fragment key={`highlighted-part-${index}`}>{part}</React.Fragment>
         ),
       )}
