365458c2eda6e8edff4d4d10f8269bffd6175155
Lucas <lucassantosamaral@hotmail.com>
2024-11-25 21:25:38 -0300

    feat: install react-testing-library and vitest

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+    include: ['./src/**/*.spec.ts*'],
+  },
   server: { port: 3000 },
 });
